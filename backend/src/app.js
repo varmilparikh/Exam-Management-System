82e22a527a4eb604c793a1cbea544863b175baba
varmil parikh <varmilparikh@gmail.com>
2026-06-23 14:25:53 +0530

    Initial project setup

---
+++ backend/src/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use(helmet());
+
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "Exam Management API"
+    });
+});
+
+export default app;
